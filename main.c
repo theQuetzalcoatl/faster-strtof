f393b38dcd3b4eb2a4f7d32cd4f27b429ff4d82a
theQuetzalcoatl <danielgerendai@gmail.com>
2021-09-02 01:50:52 +0200

    Added GPL notice

--- main.c
+++ main.c
@@ -182,3 +182,23 @@ int main(void)
 
     return 0;
 }
+
+/*
+ * main.c
+ * This file is part of faster-strtof
+ *
+ * Copyright (C) 2021 - theQuetzalcoatl
+ *
+ * faster-strtof is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License as published by
+ * the Free Software Foundation; either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * faster-strtof is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with faster-strtof. If not, see <http://www.gnu.org/licenses/>.
+ */
